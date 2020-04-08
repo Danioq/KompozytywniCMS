2be6a46ecbc705510f282dfcb1b9fb7ad1be992e
Daniel Łobacz <229363@student.pwr.edu.pl>
2020-04-08 20:02:45 +0200

    Change project structures; Add .env file with relative path to project

--- frontend/src/components/Logo/index.jsx
+++ frontend/src/components/Logo/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/logo.jpg'
+import logo from 'images/logo.jpg'
 import styled from 'styled-components';
 
 const Image = styled.img`
--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navitem from './Navitem'
-import Logo from './Logo'
+import Logo from 'components/Logo'
 import styled from 'styled-components';
 
 
